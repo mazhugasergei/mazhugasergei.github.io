e069634d49b107515625f94512764ad5f812e58c
Markus Wedler <markuswedler8@gmail.com>
2023-02-04 00:11:04 +1000

    added used technologies in works section

--- src/components/Works.js
+++ src/components/Works.js
@@ -16,6 +16,7 @@ const Works = ({ works }) => {
         works && Object.keys(works.cards).map(name => (
           <a href={ "https://" + works.cards[name].url } target="_blank" key={name}>
             <img src={"/images/works_screenshots/" + name + ".png"} />
+            <div className="using">{ works.cards[name].using }</div>
             <h3>{ works.cards[name].title }</h3>
             <p>{ works.cards[name].paragraph }</p>
           </a>
