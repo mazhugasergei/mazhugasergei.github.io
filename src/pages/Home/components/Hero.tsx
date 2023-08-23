3af54aa93e3fa0426e07c425289a8b6bec2b1339
Markus Wedler <markuswedler8@gmail.com>
2023-08-23 12:18:55 +0900

    Responsive Hero section

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -6,19 +6,24 @@ import { LocalizationContext } from "components/LocalizationContext"
 import { ReactComponent as PFP } from "assets/images/hero/pfp.svg"
 // icons
 import { FiDownload } from "react-icons/fi"
+import { FaChevronDown } from "react-icons/fa"
 
 export default () => {
   const localization = useContext(LocalizationContext)
 
   return (
     <section className="hero">
+      {/* Profile picture */}
       <PFP className="pfp" />
+      {/* Text & Buttons */}
       <div className="cont">
         <h1>{ localization.hero?.title }</h1>
         <p>{ localization.hero?.paragraph }</p>
         <a className="btn" href="mailto:ghbdtnghbdtn8@gmail.com">{ localization.hero?.contact }</a>
         <a className="btn outline" href="https://raw.githubusercontent.com/mazhugasergei/mazhugasergei.github.io_files/main/Sergei_Mazhuga_CV.pdf" download>{ localization.hero?.download_cv }<FiDownload /></a>
       </div>
+      {/* Arrow down */}
+      <a href="#about" className="arrow-down"><FaChevronDown /></a>
     </section>
   )
 }
