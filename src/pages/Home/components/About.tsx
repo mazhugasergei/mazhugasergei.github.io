573b4a172b2f7099c0e335c51a87fa176a5af534
Markus Wedler <markuswedler8@gmail.com>
2023-08-24 12:47:37 +0900

    Add skills observer

--- src/pages/Home/components/About.tsx
+++ src/pages/Home/components/About.tsx
@@ -18,19 +18,38 @@ export default () => {
       .then(data => setSkills(data.skills))
   }, [])
 
+  // skills observer
+  const skillsObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting)
+        entry.target.querySelectorAll<HTMLLIElement>('.skill').forEach(skill => {
+          skill.style.opacity = "1"
+          skill.style.transform = "none"
+        })
+    })
+  })
+
+  // observe skills
+  useEffect(()=>{
+    const skills = document.querySelector('section.about .skills')
+    skills && skillsObserver.observe(skills)
+  }, [])
+
   return (
     <section className="about" id="about">
       <SectionTitle title={ localization.about?.section_title.title } subtitle={ localization.about?.section_title.subtitle } />
       <div className="container">
+        {/* Inspiring paragraph */}
         <div className="cont">
           <Thinking className="img" />
           <h2>{ localization.about?.description.title }</h2>
           <p>{ localization.about?.description.paragraph }</p>
         </div>
+        {/* Skills list */}
         <div className="cont">
           <h2>{ localization.about?.skills.title }</h2>
           <ul className="skills">
-            { skills.map(skill => <li className="skill" key={skill}>{ skill }</li>) }
+            { skills.map((skill, i) => <li className="skill" style={{ transitionDelay: `${i * 20}ms` }} key={skill}>{ skill }</li>) }
           </ul>
         </div>
       </div>
