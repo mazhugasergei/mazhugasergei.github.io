3dc8c17fd0bac2a0e2e9967d4fb72f4bdd9decb6
Markus Wedler <markuswedler8@gmail.com>
2023-10-30 12:35:58 +1000

    Add files

--- src/components/SectionTitle.tsx
+++
@@ -1,8 +0,0 @@
-export default (props: { title: string, subtitle: string }) => {
-  return (
-    <div className="section-title">
-      <h1>{ props.title }</h1>
-      <p>{ props.subtitle }</p>
-    </div>
-  )
-}
