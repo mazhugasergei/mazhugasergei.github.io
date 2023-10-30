8b129f02fac067bf4c7e956c78fe4b011b295050
Markus Wedler <markuswedler8@gmail.com>
2023-10-30 13:27:00 +1000

    New loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,38 +2,28 @@
 import { useEffect, useRef } from "react"
 
 export default () => {
-  const contentTransition = 600
-  const loaderTransition = 600
-  const loadingProgressTransition = 750
-
   const loader = useRef<HTMLDivElement>(null)
-  const loadingProgress = useRef<HTMLDivElement>(null)
+  const loaderTransition = 600
 
   useEffect(()=>{
     const content = document.querySelector<HTMLDivElement>('.content')
 
     // initial styling
-    if(content) content.style.transition = `${contentTransition}ms`
-    if(loader.current){
-      loader.current.style.transition = `${loaderTransition}ms`
-      loader.current.style.transitionDelay = `${loadingProgressTransition}ms`
-    }
-    if(loadingProgress.current){
-      loadingProgress.current.style.transition = `${loadingProgressTransition}ms`
-      loadingProgress.current.style.width = "60%"
-    }
+    document.body.style.overflow = "hidden"
+    if(content) content.style.opacity = "0"
+    setTimeout(() => { if(content) content.style.transition = `${loaderTransition}ms` })
+    if(loader.current) loader.current.style.transition = `${loaderTransition}ms`
 
+    // onload
     const onLoad = () => {
-      if(loadingProgress.current) loadingProgress.current.style.width = "100%"
       if(loader.current) loader.current.style.opacity = "0"
       setTimeout(()=>{
         if(loader.current) loader.current.style.display = "none"
         if(content) content.style.opacity = "1"
         document.body.style.overflow = "unset"
-      }, loadingProgressTransition + loaderTransition)
+      }, loaderTransition)
     }
 
-    // onload
     if(document.readyState === "complete") onLoad()
     else{
       window.addEventListener("load", onLoad)
@@ -42,9 +32,23 @@ export default () => {
   }, [])
   
   return (
-    <div ref={loader} className="loader">
-      <div className="progress-cont">
-        <div ref={loadingProgress} className="progress" />
+    <div className="loader-cont" ref={loader}>
+      <div className="loader">
+        <svg viewBox="0 0 80 80">
+          <circle id="test" cx="40" cy="40" r="32" />
+        </svg>
+      </div>
+
+      <div className="loader triangle">
+        <svg viewBox="0 0 86 80">
+          <polygon points="43 8 79 72 7 72" />
+        </svg>
+      </div>
+
+      <div className="loader">
+        <svg viewBox="0 0 80 80">
+          <rect x="8" y="8" width="64" height="64" />
+        </svg>
       </div>
     </div>
   )
