727a607fa1e33929d397babdcea6f3ad0fad64a0
Mazhuga Sergei <ghbdtnghbdtn8@gmail.com>
2024-07-15 11:42:26 +1000

    add gallery item

--- app/(app)/gallery/page.tsx
+++ app/(app)/gallery/page.tsx
@@ -1,10 +1,30 @@
 import Post from "@/components/Post"
+import Image from "next/image"
+import Link from "next/link"
+import opticShop from "@/public/gallery/optic-shop.jpg"
 
 export default function Gallery() {
+  const items = [
+    {
+      img: opticShop,
+      href: "https://mazhugasergei.github.io/optic-shop/",
+    },
+  ]
+
   return (
     <Post>
-      <h1>Gallery</h1>
-      <section></section>
+      <h1 className="mb-4">Gallery</h1>
+      <section>
+        <ul className="grid md:grid-cols-2 gap-4">
+          {items.map(({ img, href }, i) => (
+            <li key={href}>
+              <Link target="_blank" {...{ href }} className="overflow-hidden block rounded-lg">
+                <Image src={img} alt="" placeholder="blur" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Post>
   )
 }
