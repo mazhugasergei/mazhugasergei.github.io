f2d45be2ffb59f09c62ef92782ff806d2a5d2559
Mazhuga Sergei <ghbdtnghbdtn8@gmail.com>
2024-11-12 23:15:00 +1000

    add aside

---
+++ app/components/aside.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import React from "react"
+import { headingFont } from "../fonts"
+
+export const Aside = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <aside ref={ref} {...props} className={className}>
+        <div className="h-full bg-secondary rounded-lg space-y-4 p-6">
+          <Link href="/" className={cn(headingFont.className, "grid")}>
+            <span>Mazhuga</span>
+            <span>Sergei</span>
+          </Link>
+          <Nav />
+        </div>
+      </aside>
+    )
+  }
+)
+
+export const Nav = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    const routes = [
+      {
+        name: "Home",
+        href: "/",
+      },
+      {
+        name: "Diary",
+        href: "/diary",
+      },
+      {
+        name: "Gallery",
+        href: "/gallery",
+      },
+    ]
+    return (
+      <nav ref={ref} {...props} className={className}>
+        <ul>
+          {routes.map((route) => (
+            <li key={route.name}>
+              <Link
+                href={route.href}
+                className="block leading-loose text-[0.8125rem] text-secondary-foreground hover:text-foreground transition"
+              >
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    )
+  }
+)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import { mainFont } from "@/app/fonts"
+import { Aside } from "@/app/components/aside"
+import { ThemeProvider } from "@/app/components/theme-provider"
 import "@/app/globals.css"
-import Aside from "@/components/aside"
-import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -17,10 +16,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={mainFont.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
-          <div className="grid lg:grid-cols-[15rem_1fr] xl:grid-cols-[15rem_1fr_15rem] px-2">
-            <Aside />
+          <div className="grid lg:grid-cols-[14rem_1fr] xl:grid-cols-[14rem_1fr_14rem] gap-4 p-4">
+            <Aside className="sticky top-4 h-[calc(100vh-2rem)]" />
             <main className="h-[200vh]">{children}</main>
           </div>
         </ThemeProvider>
