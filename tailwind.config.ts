e3ae5fe3e88fbb5d4270448c953f39a25118cc57
Mazhuga Sergei <ghbdtnghbdtn8@gmail.com>
2024-11-12 17:50:02 +1000

    clean up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {},
   plugins: [],
-} satisfies Config;
+} satisfies Config
