36f1930e3e74ae0212e0c69866c666284784af56
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2024-11-23 22:10:08 +1000

    update custom link

--- app/components/ui.tsx
+++ app/components/ui.tsx
@@ -10,7 +10,7 @@ interface LinkProps extends React.ComponentProps<typeof NextLink> {
 export const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
   ({ children, className, external, ...props }, ref) => {
     return (
-      <NextLink ref={ref} className={cn("inline-flex items-center underline text-blue-400", className)} {...props}>
+      <NextLink ref={ref} className={cn("inline-flex items-start underline text-blue-400", className)} {...props}>
         {children}
         {external && <ArrowUpRight size={13} />}
       </NextLink>
