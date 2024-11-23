1290b6a248b053a7f0fb3a02aa9cf795c0e116de
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2024-11-23 23:55:20 +1000

    add contact button

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,12 @@ const Intro = () => {
   return (
     <section className="pt-2">
       <div className="grid items-start gap-4 md:grid-cols-[1fr_auto]">
-        <div>
+        <div className="space-y-2">
           <h1 className={cn(headingFont.className, "text-5xl font-bold")}>
             <SymbolTransitionText text="Mazhuga Sergei" />
           </h1>
-          <p className="mt-2 text-balance">
+
+          <p className="text-balance">
             Web developer with a passion for creating visually stunning and intuitive websites. Former contributor and
             maintainer of{" "}
             <Link external target="_blank" href="https://molotov-group.ru">
@@ -26,14 +27,24 @@ const Intro = () => {
             </Link>{" "}
             public and internal projects.
           </p>
+
+          <div className="inline-grid grid-cols-2 gap-2">
+            <Link
+              href="mailto:ghbdtnghbdtn8@gmail.com"
+              className="bg-foreground px-4 py-2 font-bold text-background no-underline hover:underline"
+            >
+              Contact me
+            </Link>
+
+            <Link
+              download="Mazhuga Sergei CV"
+              href="/resume.pdf"
+              className="px-4 py-2 text-foreground no-underline hover:underline"
+            >
+              Download CV
+            </Link>
+          </div>
         </div>
-        <Link
-          download="Mazhuga Sergei CV"
-          href="/resume.pdf"
-          className="flex items-center gap-2 bg-foreground px-4 py-2 font-bold text-background no-underline hover:underline"
-        >
-          Download CV
-        </Link>
       </div>
     </section>
   )
