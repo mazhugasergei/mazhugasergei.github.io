fa573b01b8f26116eed809b581497b04fd39dc25
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2024-12-26 22:13:35 +0900

    main page title: prevent overflowing due to long string

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ const Intro = () => {
   return (
     <section className="grid gap-8 max-md:place-content-center md:grid-cols-[3fr_1fr] md:items-center">
       <div>
-        <h1 className={cn(headingFont.className, "text-5xl font-bold")}>
-          <SymbolTransitionText text="Mazhuga Sergei" />
+        <h1 className={cn(headingFont.className, "flex flex-wrap text-5xl font-bold")}>
+          <SymbolTransitionText text="Mazhuga" /> <SymbolTransitionText text="Sergei" />
         </h1>
 
         <p className="mt-3 text-balance">
