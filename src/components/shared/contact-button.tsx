d2c3d05c5f11363655ca21ca6b85e369587d9054
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-03-24 03:59:05 +1000

    refactor: move shared components files location

--- src/components/shared/contact-button.tsx
+++ src/components/shared/contact-button.tsx
@@ -1,14 +1,14 @@
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog"
 import { MailIcon, MessageCircleIcon, SendIcon } from "lucide-react"
 import Link from "next/link"
-import { Button, buttonVariants } from "../button"
+import { Button, buttonVariants } from "../ui/button"
 
 const ContactLinks = [
   {
