50e1c8f8d71d83f12c05ab0980a4b8f92ef8957e
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-06-09 17:41:27 +1000

    refactor: intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,31 @@ import { Link } from "@/components/shared/link"
 import { headingFont } from "@/fonts"
 import { cn } from "@/utils"
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="max-w-xl space-y-6">
-      <div className="animate-slide-in-x space-y-1">
-        <h1 className={cn(headingFont.className, "text-xl font-bold")}>
-          <CustomAnimatedText text="Mazhuga" /> <CustomAnimatedText text="Sergei" />
-        </h1>
-        <CustomAnimatedText text="Frontend Developer" className="text-xs" />
-      </div>
+      <Intro className="animate-slide-in-x" />
+      <Projects className="animate-slide-in-x" style={{ animationDelay: "150ms" }} />
+      <Contacts className="animate-slide-in-x" style={{ animationDelay: "300ms" }} />
+    </main>
+  )
+}
 
-      <p className="animate-slide-in-x text-xs text-balance">
+const Intro = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <section {...props}>
+      <h1 className={cn(headingFont.className, "text-xl font-bold")}>
+        <CustomAnimatedText text="Mazhuga" /> <CustomAnimatedText text="Sergei" />
+      </h1>
+
+      <CustomAnimatedText text="Frontend Developer" className="text-xs" />
+
+      <p className="mt-2 text-xs text-balance">
         A web developer based in Vladivostok, Russia, occasionally residing in Incheon, South Korea. I craft intuitive,
         purposeful websites and web services with a focus on clarity and usability. Passionate about open source and
         committed to shaping the future of the web through thoughtful, collaborative development.
       </p>
-
-      <Projects className="animate-slide-in-x" style={{ animationDelay: "150ms" }} />
-      <Contacts className="animate-slide-in-x" style={{ animationDelay: "300ms" }} />
-    </main>
+    </section>
   )
 }
 
@@ -68,7 +74,11 @@ const Contacts = (props: React.HTMLAttributes<HTMLDivElement>) => {
       <ul className="mt-2">
         {contactLinks.map(({ label, href }) => (
           <li key={label}>
-            <Link href={href} className="text-foreground/50 hover:text-foreground text-xs no-underline transition">
+            <Link
+              href={href}
+              target="_blank"
+              className="text-foreground/50 hover:text-foreground text-xs no-underline transition"
+            >
               {label}
             </Link>
           </li>
