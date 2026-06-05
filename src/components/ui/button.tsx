54c4c38d2815d735e248fffc3ad6de3ff6030e36
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-05 16:04:55 +1000

    style: change primary color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ export const buttonVariants = cva(
 	{
 		variants: {
 			variant: {
-				default: "font-bold bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+				default: "font-bold bg-foreground text-background shadow-xs hover:bg-foreground/90",
 				destructive:
 					"font-medium bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
 				outline:
