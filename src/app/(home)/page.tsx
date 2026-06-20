f8d07271c10cfc1aaff841b7905e5a03cf090935
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-20 21:41:56 +1000

    debug: remove debug menu

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,4 @@
 import { Carousel } from "@/components/carousel"
-import { Debug } from "@/components/debug"
 import { Lists } from "@/components/lists"
 import { SHOW_UI_OUTLINE } from "@/lib/constants/config"
 import { cn } from "@/utils/classname"
@@ -17,7 +16,7 @@ export default function Page() {
 				{/* left */}
 				<div className="relative lg:mr-6 lg:max-w-xl">
 					<div className="flex h-full flex-col space-y-6 py-6">
-						<Debug />
+						{/* <Debug /> */}
 						{/* <Header className="animate-slide-in-x pl-6 max-lg:pr-6" /> */}
 						{SHOW_UI_OUTLINE && <div className="bg-border animate-slide-in-x h-0.25 lg:hidden" />}
 						<Carousel
