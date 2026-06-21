114c6eabfde29fd6e9febe5d385024fa186569b5
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-21 18:31:42 +1000

    refactor: Centralize lists data

--- src/components/lists.tsx
+++ src/components/lists.tsx
@@ -1,6 +1,50 @@
 import { SoundLink } from "@/components/ui/sounds"
-import { lists } from "@/lib/constants/data"
 import { cn } from "@/utils/classname"
+
+interface List {
+	title: string
+	items: ListItem[]
+}
+
+interface ListItem {
+	name: string
+	href: string
+	year?: number
+	service?: string
+}
+
+export const lists: List[] = [
+	{
+		title: "Clients",
+		items: [
+			{ name: "Cleopatra Trading Co.", href: "https://cleopatrading.com", year: 25, service: "Web Development" },
+			{ name: "EverestAvto", href: "https://everestavtovl.ru", year: 25, service: "Web Development" },
+			{ name: "Molotov Group", href: "/archive/molotov-group", year: 24, service: "Frontend Development" },
+			{ name: "Energy Vostok", href: "/archive/energy-vostok", year: 24, service: "Frontend Development" },
+			{ name: "DVZ-TIM", href: "/archive/dvz-tim", year: 24, service: "Frontend Development" },
+			{ name: "Монтажстрой Подряд", href: "/archive/mspvl", year: 24, service: "Frontend Development" },
+			{ name: "FEFU", href: "/archive/fefu", year: 23, service: "Web Development" },
+			{ name: "STAKEME", href: "https://stakeme.pro", year: 22, service: "Web Development" },
+			{ name: "KANCOO", href: "https://www.kancoo.tech", year: 21, service: "Web Development" },
+		],
+	},
+	{
+		title: "Projects",
+		items: [
+			{ name: "Components", href: "/components", year: 26, service: "Web Development" },
+			{ name: "(moon)", href: "/the-moon", year: 25, service: "Web Development" },
+			{ name: "Dot Image Generator", href: "/dot-image-generator", year: 26, service: "Web Development" },
+			{ name: "Nimbus", href: "https://github.com/logscore/Nimbus", year: 25, service: "Frontend Contribution" },
+		],
+	},
+	{
+		title: "Contacts",
+		items: [
+			{ name: "mazhugasergei8@gmail.com", href: "mailto:mazhugasergei8@gmail.com" },
+			{ name: "@mazhugasergei", href: "https://x.com/mazhugasergei" },
+		],
+	},
+]
 
 export function Lists(props: { className?: string }) {
 	return lists.map((list, index) => (
