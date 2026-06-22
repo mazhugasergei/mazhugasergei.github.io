19aa45de46674623688ec4bff1fba7484423b358
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-22 15:29:47 +1000

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
 	output: "export",
+	trailingSlash: true,
+	skipTrailingSlashRedirect: true,
 	images: { unoptimized: true },
 }
 
