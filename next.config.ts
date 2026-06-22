22caa2e9c11d7066d900b4a4fa62b22fb1ec0d9f
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-06-22 15:00:17 +1000

    refactor: remove next js static config, wanna test if will kaboom

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-	output: "export",
-	trailingSlash: true,
-	skipTrailingSlashRedirect: true,
-	images: {
-		unoptimized: true,
-	},
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
